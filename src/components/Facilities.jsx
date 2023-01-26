49433fd6df941d683330f884e62ab541afa09e59
KahhorovSh04 <shakhzodbekkakhkhorov04@gmail.com>
2023-01-26 19:08:35 +0800

    Facilities

---
+++ src/components/Facilities.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Facilities = () => {
+  return (
+    <div id='facilities'>
+      <div className='info'>
+        <div className='line'></div>
+        <h2>Qulayliklar</h2>
+        <div className='cont'>
+          <p>
+            O'zbekiston poyezdlari O‘zbekistonda poyezdda sayohat avtomobilda
+            sayohat qilishdan ko‘ra qulayroq.
+          </p>
+          <p>
+            Ushbu jadval orqali siz Oʻzbekiston boʻylab barcha yoʻnalishlar
+            boʻyicha poyezdlar jadvalini koʻrishingiz mumkin.
+          </p>
+          <p>
+            “Afrosiyob”, “Shark” va “Nasaf” nomli poyezdlar Samarqand, Buxoro,
+            Qarshi, Farg‘ona vodiysi va Toshkent shaharlariga muntazam
+            qatnovlarni amalga oshiradi.
+          </p>
+          <p>
+            Kompaniyamiz siz uchun temir yo'l chiptalarini sotib olishi va
+            ularning elektron versiyasini ko'rsatilgan elektron pochtangizga
+            yuborishi mumkin.
+          </p>
+        </div>
+      </div>
+      <div className='bg'></div>
+    </div>
+  )
+}
+
+export default Facilities
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Hero from '../components/Hero'
 import About from '../components/About'
 import Statistics from '../components/Statistics'
 import Benefits from '../components/Benefits'
+import Facilities from '../components/Facilities'
 
 const Home = () => {
   return (
@@ -14,6 +15,7 @@ const Home = () => {
       <About />
       <Statistics />
       <Benefits />
+      <Facilities />
     </div>
   )
 }
