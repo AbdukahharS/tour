4c25f4cfa7e15c471d98d89908b998591a47eca1
KahhorovSh04 <shakhzodbekkakhkhorov04@gmail.com>
2023-01-27 19:47:14 +0800

    dropdowns

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import { MdArrowDropDown, MdShoppingCart } from 'react-icons/md'
+import { useState } from 'react'
+import { MdArrowDropUp, MdArrowDropDown, MdShoppingCart } from 'react-icons/md'
 import Logo from '../images/logo-hor.png'
 import Uz from '../images/uzb.svg'
+import En from '../images/en.svg'
+import Ru from '../images/ru.svg'
 
 const Navbar = () => {
+  const [drop, setDrop] = useState(false)
+
   return (
     <nav>
       <img src={Logo} alt='NodirahSultan' className='logo' />
@@ -11,13 +15,32 @@ const Navbar = () => {
         <button className='hover'>
           <img src={Uz} alt='flag' />
           <span>UZ</span>
-          <MdArrowDropDown />
         </button>
+        <button className='hover' onClick={() => setDrop((d) => !d)}>
+          {drop ? <MdArrowDropUp /> : <MdArrowDropDown />}{' '}
+        </button>
+        <div className={`content ${drop && 'drop'}`}>
+          <button className='hover'>
+            <img src={En} alt='flag' />
+            <span>EN</span>
+          </button>
+          <button className='hover'>
+            <img src={Ru} alt='flag' />
+            <span>RU</span>
+          </button>
+        </div>
       </div>
       <div className='links'>
-        <a href='#about' className='hover'>
-          Biz haqimizda
-        </a>
+        <div className='hoverdown'>
+          <a href='#about' className='hover'>
+            <span>Biz haqimizda</span>
+            <MdArrowDropDown />
+          </a>
+          <div className='dropdown-content'>
+            <a href='#statistics'>Statistik ma'lumotlar</a>
+            <a href='#benefits'>Umra haqida</a>
+          </div>
+        </div>
         <a href='#facilities' className='hover'>
           Qulayliklar
         </a>
