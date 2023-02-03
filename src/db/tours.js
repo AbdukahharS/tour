0c3e7033dc2b3d7455131c0355ecaffc465d73a0
KahhorovSh04 <shakhzodbekkakhkhorov04@gmail.com>
2023-02-03 20:53:10 +0800

    deleted needless tours

--- src/db/tours.js
+++ src/db/tours.js
@@ -1,8 +1,5 @@
 const tours = [
   { id: 1, name: 'Umra safari', price: 1850, img: 'umra.jpeg', entity: 1 },
-  { id: 2, name: 'Umra safari', price: 1850, img: 'umra.jpeg', entity: 1 },
-  { id: 3, name: 'Umra safari', price: 1850, img: 'umra.jpeg', entity: 1 },
-  { id: 4, name: 'Umra safari', price: 1850, img: 'umra.jpeg', entity: 1 },
 ]
 
 export default tours
