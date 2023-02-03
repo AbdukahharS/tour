da95fd683d5a40a06fcf98e513deb40cc6d0c86b
KahhorovSh04 <shakhzodbekkakhkhorov04@gmail.com>
2023-02-03 21:04:52 +0800

    Tour ad

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { CartContext } from '../context/CartContext'
 import { MdShoppingCart, MdClose } from 'react-icons/md'
 import Umrah from '../images/umra.jpeg'
@@ -6,6 +6,17 @@ import tours from '../db/tours'
 
 const Tours = () => {
   const { cart, dispatch } = useContext(CartContext)
+  const [img, setImg] = useState(null)
+
+  const handleOpen = (e, ad) => {
+    const tag = e.target.tagName
+
+    if (tag !== 'BUTTON' && tag !== 'svg' && tag !== 'path') setImg(ad)
+  }
+
+  const handleClose = () => {
+    setImg(null)
+  }
 
   const handleClick = (id) => {
     const tour = tours.filter((t) => t.id === id)
@@ -21,34 +32,54 @@ const Tours = () => {
   }
 
   return (
-    <div id='tours'>
-      <h2>
-        Tur <span>Dastur</span>
-      </h2>
-      <div className='cont'>
-        {tours.length &&
-          tours.map((tour) => (
-            <div className='card' key={tour.id}>
-              <img src={Umrah} alt='Umrah' />
-              <div className='top'>
-                <h3>{tour.name}</h3>
-                <span>{tour.price} $</span>
+    <>
+      <div id='tours'>
+        <h2>
+          Tur <span>Dastur</span>
+        </h2>
+        <div className='cont'>
+          {tours.length &&
+            tours.map((tour) => (
+              <div
+                className='card'
+                key={tour.id}
+                onClick={(e) => handleOpen(e, tour.ad)}
+                style={{ cursor: 'pointer' }}
+              >
+                <img src={Umrah} alt='Umrah' />
+                <div className='top'>
+                  <h3>{tour.name}</h3>
+                  <span>{tour.price} $</span>
+                </div>
+                <div className='bottom'>
+                  {cart.find((t) => t.id === tour.id) ? (
+                    <button onClick={() => handleRemove(tour.id)}>
+                      <MdClose />
+                    </button>
+                  ) : (
+                    <button onClick={() => handleClick(tour.id)}>
+                      <MdShoppingCart />
+                    </button>
+                  )}
+                </div>
               </div>
-              <div className='bottom'>
-                {cart.find((t) => t.id === tour.id) ? (
-                  <button onClick={() => handleRemove(tour.id)}>
-                    <MdClose />
-                  </button>
-                ) : (
-                  <button onClick={() => handleClick(tour.id)}>
-                    <MdShoppingCart />
-                  </button>
-                )}
-              </div>
-            </div>
-          ))}
+            ))}
+        </div>
       </div>
-    </div>
+      <div
+        className='modal'
+        style={{ display: img !== null ? 'flex' : 'none' }}
+      >
+        <div className='cont'>
+          {img !== null && (
+            <img src={require(`../images/ads/${img}`)} alt='Gallery' />
+          )}
+          <button className='close' onClick={handleClose}>
+            <MdClose />
+          </button>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/db/tours.js
+++ src/db/tours.js
@@ -1,5 +1,5 @@
 const tours = [
-  { id: 1, name: 'Umra safari', price: 1850, img: 'umra.jpeg', entity: 1 },
+  { id: 1, name: 'Umra safari', price: 1850, img: 'umra.jpeg', entity: 1, ad: 'umrah-ad.jpg' },
 ]
 
 export default tours
